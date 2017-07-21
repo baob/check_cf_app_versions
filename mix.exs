c590dd1c6c6829538817b4281a8a484746bd6dfd
Andy Roberts <andy.roberts@macmillan.com>
2017-07-21 18:28:04 +0100

    add dotenv for test and dev evironments ...
    ... after taking into account warning at
    https://github.com/avdi/dotenv_elixir

--- mix.exs
+++ mix.exs
@@ -19,7 +19,15 @@ defmodule CheckAppVersions.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: app_list(Mix.env)]
+  end
+
+  defp app_list(:dev),  do: [:dotenv | app_list]
+  defp app_list(:test), do: [:dotenv | app_list]
+  defp app_list(_),     do: app_list
+
+  defp app_list do
+    [:logger, :httpoison]
   end
 
   # Dependencies can be Hex packages:
@@ -32,6 +40,9 @@ defmodule CheckAppVersions.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   def deps do
-    [{:httpoison, "~> 0.12"}]
+    [
+      {:httpoison, "~> 0.12"},
+      {:dotenv, "~> 2.0.0"}
+    ]
   end
 end
